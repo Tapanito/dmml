1a77d454ce283b29dee01a68b1480d2fbdc83006
Vytautas <vtumas@gmail.com>
2015-11-24 22:23:57 +0000

    edits

--- cw2/code/construct_optall.py
+++ cw2/code/construct_optall.py
@@ -1,13 +1,23 @@
 import ppc
 import sys
+import numpy as np
 
-def main():
+def column(matrix, i):
+    return [row[i] for row in matrix]
+
+def main(args):
     filename = "../opt{0}.txt"
+    script, size = args
     tmp = []
     for i in range(10):
         tmp.append(ppc.main(filename.format(str(i))))
-
     
+    lines = ppc.open_csv(filename.format(str(0)))
+    lines = ppc.to_int(lines)
+    qq = column(lines, tmp[0][tmp[0].keys()[0]])
+
+
+
 
 if __name__ == "__main__":
-    main() 
+    main(sys.argv) 
