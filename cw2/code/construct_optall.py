ce166bac5e6b01725a7a9f46d876235a487872a7
Vytautas <vtumas@gmail.com>
2015-11-25 10:22:57 +0000

    édits

--- cw2/code/construct_optall.py
+++ cw2/code/construct_optall.py
@@ -9,15 +9,13 @@ def main(args):
     filename = "../opt{0}.txt"
     script, size = args
     tmp = []
-    for i in range(10):
+    for i in range(1):
         tmp.append(ppc.main(filename.format(str(i))))
-    
+    print tmp[0]    
     lines = ppc.open_csv(filename.format(str(0)))
     lines = ppc.to_int(lines)
-    qq = column(lines, tmp[0][tmp[0].keys()[0]])
-
-
-
+    qq = column(lines, tmp[0][0][0])
+    print qq
 
 if __name__ == "__main__":
     main(sys.argv) 
